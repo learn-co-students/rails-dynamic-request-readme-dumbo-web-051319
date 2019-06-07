727f20b8ceb565e50133f7c0a26328d9213dabb3
Tanuka16 <das.tanuka@yahoo.com>
2019-06-07 15:48:21 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  #create a static request, which allows localhost:3000/about to run
+  # get 'about', to: "static#about"
+  resources :posts, only: :show
+  # mapped to the static controller and its about action, which renders the about.html.erb view template
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
